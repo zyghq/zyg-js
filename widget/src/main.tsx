d7d545e29d39833bf3164fbb53b067c94d832fd4
Sanchit Rk <sanchitrrk@gmail.com>
2024-09-26 19:07:03 +0530

    create lib component.

--- widget/src/main.tsx
+++ widget/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { WidgetContainer } from "@lib/container.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <WidgetContainer>
+      <App />
+    </WidgetContainer>
   </StrictMode>
 );
